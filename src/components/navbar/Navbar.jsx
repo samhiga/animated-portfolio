7d8a476e1f0ba6fbd96d4e48d091f3f2215b9fa2
samhiga <samhiga18@gmail.com>
2023-11-02 18:00:57 -0700

    installed framermotion and began sidebar work

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,17 @@
-import "./navbar.scss"
+import "./navbar.scss";
+import { motion } from "framer-motion";
+import Sidebar from "../sidebar/Sidebar";
 
 const Navbar = () => {
     return (
         <div className="navbar">
+            <Sidebar/>
             <div className="wrapper">
-                <span>Sam Higa</span>
+                <motion.span
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                tranistion={{ duration: 0.5 }}
+                >Sam Higa</motion.span>
                 <div className="social">
                     <a href="#"><img src ="/linkedin.png" alt="" /></a>
                     <a href="#"><img src ="/facebook.png" alt="" /></a>
