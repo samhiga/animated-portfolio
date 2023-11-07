f10570bbe50c62c5c289b350247d7f63b881d9a9
samhiga <samhiga18@gmail.com>
2023-11-06 16:32:25 -0800

    added animated sidebar

---
+++ src/components/sidebar/links/links.jsx
@@ -0,0 +1,16 @@
+const Links = () => {
+
+const items = [
+    "Homepage",
+    "About",
+    "Portfolio",
+    "Contact",
+    "Homepage",
+
+]
+    return (
+        <div className="links">Links</div>
+    )
+}
+
+export default Links;
